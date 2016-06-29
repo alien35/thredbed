189899bc728019f6a72225b3e458a4156489b7ea
Alexander Leon <aleon6@u.rochester.edu>
2016-06-29 08:38:52 -0400

    add paperclip image

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'haml'
 gem 'devise'
 gem 'html2haml'
+gem 'paperclip', '~> 4.2'
 
 group :development, :test do
   gem 'byebug'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  has_attached_file :image, styles: { medium: "260x", thumb: "100x"},
+                         default_url: 'question-mark-comlink.jpg'
+  validates_attachment_content_type :image,
+                                     content_type: /\Aimage\/.*\Z/
+
 end

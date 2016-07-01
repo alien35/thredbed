0194d2f732c40fddb1b436e30ed49e0559617f2e
Alexander Leon <aleon6@u.rochester.edu>
2016-07-01 19:00:37 -0400

    improve profile pic show page

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -13,5 +13,11 @@ module ProfilesHelper
                                     class: 'index-profile-image'
   end
 
+  def show_profile_avatar_select(user)
+    return image_tag user.avatar.url(:thumb),
+                     class: 'index-profile-image' if user.avatar.exists?
+    image_tag 'blank_profile_pic.png',
+                                    class: 'show-profile-image'
+  end
 
 end

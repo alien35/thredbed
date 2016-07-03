ae78ba63384523c0c689b7b2cee49b8257774a04
Alexander Leon <aleon6@u.rochester.edu>
2016-07-03 09:16:09 -0400

    set up preliminary follow/unfollow

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -20,4 +20,9 @@ module ProfilesHelper
                                     class: 'show-profile-image'
   end
 
+  def current_user_is_following(current_user_id, followed_user_id)
+    relationship = Follow.find_by(follower_id: current_user_id, following_id: followed_user_id)
+    return true if relationship
+  end
+
 end

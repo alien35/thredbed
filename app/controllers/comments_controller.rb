abcda597c0c0e13ec6a02300852d9f7685b56426
Alexander Leon <aleon6@u.rochester.edu>
2016-07-04 18:08:40 -0400

    fixes comment problem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
 
     if @comment.save
-      create_notification @post
+      create_notification @post, @comment
       respond_to do |format|
         format.html { redirect_to root_path }
         format.js
@@ -52,15 +52,14 @@ class CommentsController < ApplicationController
 
   private
 
-    def create_notification(post)
-      return if post.user.id == current_user.id
-      Notification.create(user_id: post.user.id,
+    def create_notification(post, comment)
+    return if post.user.id == current_user.id
+    Notification.create(user_id: post.user.id,
                         notified_by_id: current_user.id,
                         post_id: post.id,
-                        comment_id: @comment.id,
+                        identifier: comment.id,
                         notice_type: 'comment')
-    end
-    end
+  end
 
     def create_upvote_notification(post)
       Notification.create(user_id: post.user.id,

aee413b83db2d392eb8985a410b8f32b2586abec
Alexander Leon <aleon6@u.rochester.edu>
2016-07-05 00:59:34 -0400

    gonna try alternative following method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/index'
 
-  post ':user_name/follow_user',   to: 'relationships#follow_user',   as: :follow_user
+  post ':user_name/follow_user', to: 'relationships#follow_user', as: :follow_user
   post ':user_name/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
   get 'notifications/:id/link_through', to: 'notifications#link_through', as: :link_through
   get 'notifications', to: 'notifications#index'

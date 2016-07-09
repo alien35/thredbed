8db40f005c0b11bab6117c30a12f93ea1eece330
Alexander Leon <aleon6@u.rochester.edu>
2016-07-09 12:37:26 +0800

    update email setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://evening-ridge-66378.herokuapp.com'
+  host = 'https://gentle-woodland-91538.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',

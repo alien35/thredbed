645f5709b3470f69377f46b856034f8b344c02ae
Alexander Leon <aleon6@u.rochester.edu>
2016-07-11 16:45:12 +0800

    Improve comments algor.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   acts_as_votable
+  scope :of_followed_users, -> (following_users) { where user_id: following_users }
 
   validates :content, presence: true
   belongs_to :user

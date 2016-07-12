e86f4c55c8fd28a1f8f81da1e48a31bb3943009a
Alexander Leon <aleon6@u.rochester.edu>
2016-07-12 21:08:26 +0800

    improve reply buttons

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,19 +1,7 @@
-$(document).ready(function(){
-    //Check if the current URL contains '#'
-    if(document.URL.indexOf("#")==-1){
-        // Set the URL to whatever it was plus "#".
-        url = document.URL+"#";
-        location = "#";
 
-        //Reload the page
-        location.reload(true);
-    }
-});
 
 $(document).ready(function() {
-  function myFunction() {
-    location.reload();
-}
+
   $(".reply_button").click(function () {
       $(this).siblings('.response_form_hide').show();
     });

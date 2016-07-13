e807d6bc88027b0dfe8421248a48e406b5d94c06
Alexander Leon <aleon6@u.rochester.edu>
2016-07-13 12:30:28 +0800

    improve response form

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -20,6 +20,13 @@ module ProfilesHelper
                                     class: 'show-profile-image'
   end
 
+  def reply_profile_avatar_select(user)
+    return image_tag user.avatar.url(:thumb),
+                     class: 'reply-profile-image' if user.avatar.exists?
+    image_tag 'blank_profile_pic.png',
+                                    class: 'reply-profile-image'
+  end
+
   def current_user_is_following(current_user_id, followed_user_id)
     relationship = Follow.find_by(follower_id: current_user_id, following_id: followed_user_id)
     return true if relationship

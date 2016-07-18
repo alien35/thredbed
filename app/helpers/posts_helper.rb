e4102114bb7c5ca7923eeba1beb897feef0cff13
Alexander Leon <aleon6@u.rochester.edu>
2016-07-18 21:54:59 +0800

    small things2

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,13 +1,11 @@
 module PostsHelper
 
-  def title_max_length(str)
-    str.split("").length > 100 ?
-    str.split("")[0...100].join("") + "..." : str
-  end
 
-  def caption_max_length(str)
+
+  def index_commentary_shrink(str)
     str.split("").length > 300 ?
     str.split("")[0...300].join("") + "..." : str
+    str.gsub("<br>", " ")
   end
 
   def likers_of(post)

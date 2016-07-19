6e6bfa5756c77e2f9553d641325c5c56f8f058ce
Alexander Leon <aleon6@u.rochester.edu>
2016-07-19 20:57:02 +0800

    remove 'render with scope'

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,7 @@ def create
         build_resource
         clean_up_passwords(resource)
         flash[:alert] = "There was an error with the recaptcha code below. Please re-enter the code and click submit."
-        render_with_scope :new
+        render :new
       end
     end
 

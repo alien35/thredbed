a90a9f23b5c2da8179b25837f98d94e327a6aa2a
Alexander Leon <aleon6@u.rochester.edu>
2016-07-22 09:40:15 +0800

    improve search

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,7 +22,7 @@ class Post < ActiveRecord::Base
                                      content_type: /\Aimage\/.*\Z/
 
   def self.search(query)
-    joins(:tags).where("lower(commentary) LIKE :search OR lower(link) LIKE :search OR lower(tags.name) LIKE :search", search: "%#{query.downcase}%")
+    joins(:tags).where("commentary LIKE :search OR link LIKE :search OR tags.name LIKE :search", search: "%#{query}%")
   end
 
   #def ends_with_q

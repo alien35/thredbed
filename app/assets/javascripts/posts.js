e728234e0429597fec6c44a388d1b7fb8c0e2f51
Alexander Leon <aleon6@u.rochester.edu>
2016-07-23 23:46:00 +0800

    improve search

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -5,3 +5,12 @@ $(function () {
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
+
+$('#search').keypress(function (e) {
+    var str = $('#search').val();
+    var domain = "http://www.thredbed.com";
+    var url = domain+"default.aspx?search=" + str;
+    if (e.keyCode == 13) {
+        location.href = url;
+    }
+});

3edaa37587782ae7153fb73e643a42ee0e66c8ef
Alexander Leon <aleon6@u.rochester.edu>
2016-07-27 21:57:37 +0800

    improve borders for follow count

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,8 +3,12 @@ module UsersHelper
   def follow_num(post_user)
     if post_user.followers.count < 25
       "follow_0"
-    else
+    elsif post_user.followers.count < 50
       "follow_25"
+    elsif post_user.followers.count < 100
+      "follow_50"
+    else
+      "follow_100"
     end
   end
 
